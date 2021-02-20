52b04205ba35283daf53973d708ab5b7782498a2
eudescam <ecamacho@bananacode.co>
2021-02-19 22:06:45 -0600

    Attempt heroku build

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 
+        'mongodb+srv://admin:W4qTQK5A0X1CE5rI@cluster0.yvwmi.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
